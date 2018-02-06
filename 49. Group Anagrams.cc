489bfafd1650136031d75aacac53caa08af4f1be
jiangzhe112 <982744091@qq.com>
2018-02-06 23:37:49 +0800

    Update 49. Group Anagrams.cc

--- 49. Group Anagrams.cc
+++ 49. Group Anagrams.cc
@@ -1,3 +1,16 @@
+/*
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note: All inputs will be in lower-case.
+*/
 #include <vector>
 #include <iostream>
 #include <algorithm>
