beb08efd0ddda8cc98bb57e4adbd503f07a9e355
jiangzhe112 <982744091@qq.com>
2018-02-06 23:38:59 +0800

    Update 100. Same Tree.cc

--- 100. Same Tree.cc
+++ 100. Same Tree.cc
@@ -2,6 +2,38 @@
 #include <iostream>
 
 /*
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
 比较两棵 树 是否相等 
 
 这种简单的 迭代还是写的出来的
